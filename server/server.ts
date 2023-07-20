adaf442871b36deaa6539eb8c3550dc667de9edd
Lauren Lancaster <737lanc@gmail.com>
2023-07-20 13:17:27 +1200

    set up back end

--- server/server.ts
+++ server/server.ts
@@ -1,9 +1,11 @@
 import express from 'express'
 import path from 'path'
+import display from './routes/display'
 
 const server = express()
 
 server.use(express.json())
 server.use(express.static(path.join(__dirname, 'public')))
+server.use('api/v1/display', display)
 
 export default server
